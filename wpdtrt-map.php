852776b45947212cea7dfccf161443d66bdf2b90
Dan Smith <dev@dotherightthing.co.nz>
2018-04-23 10:32:55 +1200

    Use latest beta version of ACF 5

--- wpdtrt-map.php
+++ wpdtrt-map.php
@@ -92,6 +92,14 @@ if( ! defined( 'WPDTRT_MAP_URL' ) ) {
 }
 
 /**
+ * Enable ACF 5 early access
+ * Requires at least version ACF 4.4.12 to work
+ */
+if( ! defined( 'ACF_EARLY_ACCESS' ) ) {
+  define('ACF_EARLY_ACCESS', '5');
+}
+
+/**
  * Include plugin logic
  *
  * @version   0.0.1
