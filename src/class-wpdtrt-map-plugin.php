8900e7e9b5892bac84ad1910a1e5377c85656a4d
Dan Smith <dev@dotherightthing.co.nz>
2021-01-29 00:11:40 +1300

    Update wpdtrt-plugin-boilerplate from 1.7.13 to 1.7.14

--- src/class-wpdtrt-map-plugin.php
+++ src/class-wpdtrt-map-plugin.php
@@ -16,7 +16,7 @@
  * Since:
  *   0.8.13 - DTRT WordPress Plugin Boilerplate Generator
  */
-class WPDTRT_Map_Plugin extends DoTheRightThing\WPDTRT_Plugin_Boilerplate\r_1_7_13\Plugin {
+class WPDTRT_Map_Plugin extends DoTheRightThing\WPDTRT_Plugin_Boilerplate\r_1_7_14\Plugin {
 
 	/**
 	 * Constructor: __construct
